cbdd4507f1455570d4ef34e73fa4610f6aace141
Andrew Wyper <andy_wyper@yahoo.co.uk>
2020-02-26 16:05:12 +0000

    whiskies by region working

--- src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository/WhiskyRepository.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository/WhiskyRepository.java
@@ -11,4 +11,6 @@ public interface WhiskyRepository extends JpaRepository<Whisky, Long>, WhiskyRep
     List<Whisky> findByYear(int year);
 
     List<Whisky> findByDistilleryNameAndAge(String name, int age);
+
+    List<Whisky> findByDistilleryRegion(String region);
 }
